7a28877062d36c79f48cd461771d5ce4b86d2d42
tgajdoski <t.gajdoski@gmail.com>
2018-04-20 12:04:41 +0200

    moved to ts - webpack - deleted client app

---
+++ functions/index.ts
@@ -0,0 +1,28 @@
+import * as express from 'express';
+import * as cors from 'cors';
+import * as functions from 'firebase-functions';
+import * as apolloServerExpress from 'apollo-server-express';
+import * as schemaPrinter from 'graphql/utilities/schemaPrinter';
+import * as schema from './graphql/schema';
+
+const app = express();
+// BODYPARSER IS ALREADY IMPLEMENTED BY FIREBASE
+app.use(cors());
+app.options('*', cors());
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  return next();
+});
+app.use(
+  '/graphql',
+  apolloServerExpress.graphqlExpress({ schema })
+);
+app.use(
+  '/graphiql',
+  apolloServerExpress.graphiqlExpress({ endpointURL: '/api/graphql' })
+);
+app.use('/schema', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send(schemaPrinter.printSchema(schema));
+});
+exports.api = functions.https.onRequest(app);
--- public/client-apollo/src/apis/apolloClient.js
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, createNetworkInterface } from 'react-apollo';
-import { ENDPOINT } from '../strings';
-
-const networkInterface = createNetworkInterface({
-  uri: ENDPOINT,
-});
-export default new ApolloClient({
-  networkInterface,
-});
